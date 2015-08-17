8d1c5b172f09217f4ef798f7e54897aa186a14ec
Joe12o <joe.298@live.com>
2015-08-17 01:21:59 -0400

    Pass NetHandlerLoginServer along with PlayerAuthenticateEvent

--- src/main/java/com/skcraft/plume/asm/PlumeTransformer.java
+++ src/main/java/com/skcraft/plume/asm/PlumeTransformer.java
@@ -1,11 +1,15 @@
 package com.skcraft.plume.asm;
 
 import net.minecraft.launchwrapper.IClassTransformer;
+import net.minecraft.launchwrapper.Launch;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.commons.AdviceAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.objectweb.asm.Opcodes.ASM5;
 
@@ -50,10 +54,27 @@ public class PlumeTransformer implements IClassTransformer {
                 mv.visitTypeInsn(NEW, "com/skcraft/plume/event/network/PlayerAuthenticateEvent");
                 mv.visitInsn(DUP);
                 mv.visitVarInsn(ALOAD, 1);
-                mv.visitMethodInsn(INVOKESPECIAL, "com/skcraft/plume/event/network/PlayerAuthenticateEvent", "<init>", "(Lcom/mojang/authlib/GameProfile;)V", false);
+                mv.visitVarInsn(ALOAD, 0);
+                mv.visitFieldInsn(GETFIELD, ObfMappings.get("net/minecraft/server/network/NetHandlerLoginServer$1"), "this$0", "L" + ObfMappings.get("net/minecraft/server/network/NetHandlerLoginServer") + ";");
+                mv.visitMethodInsn(INVOKESPECIAL, "com/skcraft/plume/event/network/PlayerAuthenticateEvent", "<init>", "(Lcom/mojang/authlib/GameProfile;L" + ObfMappings.get("net/minecraft/server/network/NetHandlerLoginServer") + ";)V", false);
                 mv.visitMethodInsn(INVOKEVIRTUAL, "cpw/mods/fml/common/eventhandler/EventBus", "post", "(Lcpw/mods/fml/common/eventhandler/Event;)Z", false);
                 mv.visitInsn(POP);
             }
+        }
+    }
+
+    private static final class ObfMappings {
+
+        public static final Map<String, String> mappings = new HashMap<>();
+        private static final boolean devEnvironment = (Boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
+
+        public static String get(String key) {
+            return devEnvironment ? key : mappings.get(key);
+        }
+
+        static {
+            mappings.put("net/minecraft/server/network/NetHandlerLoginServer", "nn");
+            mappings.put("net/minecraft/server/network/NetHandlerLoginServer$1", "no");
         }
     }
 }
--- src/main/java/com/skcraft/plume/event/network/PlayerAuthenticateEvent.java
+++ src/main/java/com/skcraft/plume/event/network/PlayerAuthenticateEvent.java
@@ -2,16 +2,23 @@ package com.skcraft.plume.event.network;
 
 import com.mojang.authlib.GameProfile;
 import cpw.mods.fml.common.eventhandler.Event;
+import net.minecraft.server.network.NetHandlerLoginServer;
 
 public class PlayerAuthenticateEvent extends Event {
 
     private final GameProfile profile;
+    private final NetHandlerLoginServer netHandler;
 
-    public PlayerAuthenticateEvent(GameProfile profile) {
+    public PlayerAuthenticateEvent(GameProfile profile, NetHandlerLoginServer netHandler) {
         this.profile = profile;
+        this.netHandler = netHandler;
     }
 
     public GameProfile getProfile() {
         return profile;
+    }
+
+    public NetHandlerLoginServer getNetHandler() {
+        return netHandler;
     }
 }
