1ad200309cfc015d42802f4e2ef887f69cd2b9d9
Joe12o <joe.298@live.com>
2015-08-18 19:50:42 -0400

    Simplified authentication event listener

--- src/main/java/com/skcraft/plume/listener/UserManagementListener.java
+++ src/main/java/com/skcraft/plume/listener/UserManagementListener.java
@@ -14,25 +14,15 @@ public class UserManagementListener {
     public void onAuthenticate(PlayerAuthenticateEvent event) {
         List<Ban> bans = Plume.INSTANCE.getBanManager().findActiveBans(new UserId(event.getProfile().getId()));
         if(bans != null && !bans.isEmpty()) {
-            Ban latest = null;
-            for (Ban ban : bans) {
-                if (latest == null) {
-                    latest = ban;
-                } else if (ban.getExpireTime() == null) {
-                    latest = ban;
-                    break;
-                } else if (ban.getExpireTime().compareTo(latest.getExpireTime()) > 0) {
-                    latest = ban;
-                }
-            }
+            bans.sort((Ban ban1, Ban ban2) -> ban1.getExpireTime() == null ? 1 : ban2.getExpireTime() == null ? -1 : ban1.getExpireTime().compareTo(ban2.getExpireTime()));
 
+            Ban latest = bans.get(bans.size() - 1);
             if (latest != null) {
                 StringBuilder builder = new StringBuilder();
                 builder.append("Your access has been suspended. To appeal, mention #");
                 builder.append(latest.getId());
                 if (latest.getExpireTime() != null) {
-                    builder.append("\n");
-                    builder.append("Expires: ");
+                    builder.append("\nExpires: ");
                     builder.append(latest.getExpireTime());
                 }
                 event.getNetHandler().func_147322_a(builder.toString());
