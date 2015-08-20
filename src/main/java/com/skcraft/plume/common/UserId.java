29c3dda657ed7366b4ece613e9c4d10c22e3373a
sk89q <the.sk89q@gmail.com>
2015-08-19 19:47:18 -0700

    Make UserID.UUID final.

--- src/main/java/com/skcraft/plume/common/UserId.java
+++ src/main/java/com/skcraft/plume/common/UserId.java
@@ -3,23 +3,24 @@ package com.skcraft.plume.common;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.annotation.Nullable;
 import java.util.UUID;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 @Data
 @EqualsAndHashCode(of = "uuid")
 public class UserId {
 
-    private UUID uuid;
-    private String name;
-
-    public UserId() {
-    }
+    private final UUID uuid;
+    @Nullable private String name;
 
     public UserId(UUID uuid) {
-        this.uuid = uuid;
+        this(uuid, null);
     }
 
     public UserId(UUID uuid, String name) {
+        checkNotNull(uuid, "uuid");
         this.uuid = uuid;
         this.name = name;
     }
