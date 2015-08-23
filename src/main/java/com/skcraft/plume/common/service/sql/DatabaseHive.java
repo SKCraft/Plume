14d5d0af92f1cda43888ac69156815965fade55e
sk89q <the.sk89q@gmail.com>
2015-08-23 02:43:07 -0700

    Set Group autoJoin property on load.

--- src/main/java/com/skcraft/plume/common/service/sql/DatabaseHive.java
+++ src/main/java/com/skcraft/plume/common/service/sql/DatabaseHive.java
@@ -64,6 +64,7 @@ public class DatabaseHive implements Hive {
                 group.setId(record.getId());
                 group.setName(record.getName());
                 group.setPermissions(ImmutableSet.copyOf(record.getPermissions().toLowerCase().split("\n")));
+                group.setAutoJoin(record.getAutoJoin() == 1);
                 groupsBuilder.put(group.getId(), group);
             }
 
