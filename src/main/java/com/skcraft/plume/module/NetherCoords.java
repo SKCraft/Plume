380852c61f8e05e148027fd30704becea6a3c425
LeiserGeist <leisergeist@gmail.com>
2015-08-25 03:22:33 -0400

    Added /nethercoords (oops on last commit)

--- src/main/java/com/skcraft/plume/module/NetherCoords.java
+++ src/main/java/com/skcraft/plume/module/NetherCoords.java
@@ -1,7 +1,44 @@
 package com.skcraft.plume.module;
 
-/**
- * Created by LeiserGeist on 8/25/2015.
- */
+import com.sk89q.intake.Command;
+import com.sk89q.intake.Require;
+import com.skcraft.plume.command.Sender;
+import com.skcraft.plume.common.util.module.Module;
+import com.skcraft.plume.util.Messages;
+import lombok.extern.java.Log;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ChatComponentText;
+
+import static com.skcraft.plume.common.util.SharedLocale.tr;
+
+
+@Module(name = "nethercoords-commands")
+@Log
 public class NetherCoords {
+    @Command(aliases = "nethercoords", desc = "Displays your corresponding nether or overworld coords")
+    @Require("plume.nethercoords")
+    public void nethercoords(@Sender ICommandSender sender) {
+        if(sender instanceof EntityPlayerMP) {
+            EntityPlayerMP player = (EntityPlayerMP) sender;
+
+            switch (player.getEntityWorld().provider.dimensionId) {
+                case -1: //nether
+                    sender.addChatMessage(Messages.info(tr("nethercoords.overworld",
+                            Math.floor(player.posX * 8), Math.floor(player.posY), Math.floor(player.posZ * 8))));
+                    break;
+                case 0: //overworld
+                    sender.addChatMessage(Messages.info(tr("nethercoords.nether",
+                            Math.floor(player.posX / 8), Math.floor(player.posY), Math.floor(player.posZ / 8))));
+                    break;
+                default: //any other
+                    sender.addChatMessage(Messages.info(tr("nethercoords.nether",
+                            Math.floor(player.posX / 8), Math.floor(player.posY), Math.floor(player.posZ / 8))));
+                    break;
+            }
+        } else {
+            sender.addChatMessage(Messages.error(tr("messages.playerRequired")));
+        }
+    }
+
 }
