1fa5e64fd3fcf59b1f2637296749bb1552b67068
Nentify <nentify@live.co.uk>
2015-09-01 18:58:28 +0100

    Clean up Status

--- src/main/java/com/skcraft/plume/module/Status.java
+++ src/main/java/com/skcraft/plume/module/Status.java
@@ -3,6 +3,7 @@ package com.skcraft.plume.module;
 import com.sk89q.intake.Command;
 import com.sk89q.intake.Require;
 import com.sk89q.intake.parametric.annotation.Optional;
+import com.sk89q.intake.parametric.annotation.Switch;
 import com.skcraft.plume.command.Sender;
 import com.skcraft.plume.common.util.SharedLocale;
 import com.skcraft.plume.common.util.module.Module;
@@ -12,6 +13,7 @@ import com.skcraft.plume.util.Server;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 
@@ -27,79 +29,65 @@ public class Status {
 
     private final Set<UUID> gods = new HashSet<>();
 
-    public static EntityPlayer getTarget(ICommandSender sender, String name) {
-        EntityPlayer target;
-
-        if (name != null) {
-            target = Server.findPlayer(name);
-
-            if (target == null) {
-                sender.addChatMessage(Messages.error(tr("playerNotFound", name)));
-                return null;
-            }
-        } else {
-            if (sender instanceof EntityPlayer) {
-                target = (EntityPlayer) sender;
-            } else {
-                sender.addChatMessage(Messages.error(tr("playerRequired")));
-                return null;
-            }
-        }
-
-        return target;
-    }
-
     @Command(aliases = "heal", desc = "Regain full health points")
     @Require("plume.status.heal")
-    public void heal(@Sender ICommandSender sender, @Optional String name, @Optional Integer amount) {
-        EntityPlayer target = getTarget(sender, name);
-
-        if (target != null) {
-            float health = amount != null ? target.getMaxHealth() + amount : 20;
-            target.setHealth(health);
-            target.addChatMessage(Messages.info(tr("status.heal", target.getDisplayName() + "'s")));
+    public void heal(@Sender ICommandSender sender, @Optional EntityPlayerMP target, @Switch('a') Integer amount) {
+        if (!(sender instanceof EntityPlayerMP) && target == null) {
+            System.out.println("nope..");
+            return;
         }
+
+        EntityPlayerMP player = target != null ? target : (EntityPlayerMP) sender;
+        float health = amount != null ? player.getHealth() + amount : player.getMaxHealth();
+
+        player.setHealth(health);
+        player.addChatMessage(Messages.info(tr("status.heal")));
     }
 
     @Command(aliases = "feed", desc = "Regain full hunger")
     @Require("plume.status.feed")
-    public void feed(@Sender EntityPlayer sender, @Optional String name, @Optional Integer amount) {
-        EntityPlayer target = getTarget(sender, name);
-
-        if (target != null) {
-            Integer feed = amount != null ? amount : 20;
-            target.getFoodStats().addStats(feed, 10);
-            target.addChatMessage(Messages.info(tr("status.feed")));
+    public void feed(@Sender ICommandSender sender, @Optional EntityPlayerMP target, @Switch('a') Integer amount) {
+        if (!(sender instanceof EntityPlayerMP) && target == null) {
+            System.out.println("nope..");
+            return;
         }
+
+        EntityPlayerMP player = target != null ? target : (EntityPlayerMP) sender;
+        Integer food = amount != null ? amount : 20;
+
+        player.getFoodStats().addStats(food, 10);
+        player.addChatMessage(Messages.info(tr("status.feed")));
     }
 
     @Command(aliases = "god", desc = "Enable god mode")
     @Require("plume.status.god")
-    public void god(@Sender ICommandSender sender, @Optional String name) {
-        EntityPlayer target = getTarget(sender, name);
+    public void god(@Sender ICommandSender sender, @Optional EntityPlayerMP target) {
+        if (!(sender instanceof EntityPlayerMP) && target == null) {
+            System.out.println("nope..");
+            return;
+        }
 
-        if (target != null) {
-            if (!gods.contains(target.getUniqueID())) {
-                gods.add(target.getUniqueID());
-                sender.addChatMessage(Messages.info(tr("status.god.on", target.getDisplayName())));
-            } else {
-                sender.addChatMessage(Messages.error(tr("status.god.already", target.getDisplayName())));
-            }
+        if (!gods.contains(target.getUniqueID())) {
+            gods.add(target.getUniqueID());
+            sender.addChatMessage(Messages.info(tr("status.god.on", target.getDisplayName())));
+        } else {
+            sender.addChatMessage(Messages.error(tr("status.god.already", target.getDisplayName())));
         }
     }
 
     @Command(aliases = "ungod", desc = "Disable god mode")
     @Require("plume.status.god")
-    public void ungod(@Sender ICommandSender sender, @Optional String name) {
-        EntityPlayer target = getTarget(sender, name);
+    public void ungod(@Sender ICommandSender sender, @Optional EntityPlayerMP target) {
+        if (!(sender instanceof EntityPlayerMP) && target == null) {
+            System.out.println("nope..");
+            return;
+        }
 
-        if (target != null) {
-            if (gods.contains(target.getUniqueID())) {
-                gods.remove(target.getUniqueID());
-                sender.addChatMessage(Messages.info(tr("status.god.off", target.getDisplayName())));
-            } else {
-                sender.addChatMessage(Messages.error(tr("status.god.not", target.getDisplayName())));
-            }
+        if (gods.contains(target.getUniqueID())) {
+            gods.remove(target.getUniqueID());
+            sender.addChatMessage(Messages.info(tr("status.god.off", target.getDisplayName())));
+        } else {
+            sender.addChatMessage(Messages.error(tr("status.god.not", target.getDisplayName())));
         }
     }
 
