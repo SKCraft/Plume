cabcaa7d9e2d07cdd5b697597e4c55770557590e
Albert Pham <the.sk89q@gmail.com>
2015-09-01 10:26:21 -0700

    Add set-of-players as a possible command parameter type.

--- src/main/java/com/skcraft/plume/command/ForgeModule.java
+++ src/main/java/com/skcraft/plume/command/ForgeModule.java
@@ -3,20 +3,25 @@ package com.skcraft.plume.command;
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.google.common.reflect.TypeToken;
 import com.sk89q.intake.argument.ArgumentException;
 import com.sk89q.intake.argument.ArgumentParseException;
 import com.sk89q.intake.argument.CommandArgs;
 import com.sk89q.intake.parametric.AbstractModule;
+import com.sk89q.intake.parametric.Key;
 import com.sk89q.intake.parametric.Provider;
 import com.sk89q.intake.parametric.ProvisionException;
+import com.skcraft.plume.util.Server;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.server.MinecraftServer;
 
 import javax.annotation.Nullable;
 import java.lang.annotation.Annotation;
+import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 import static com.skcraft.plume.common.util.SharedLocale.tr;
 
@@ -31,6 +36,32 @@ public class ForgeModule extends AbstractModule {
         bind(EntityPlayerMP.class).annotatedWith(Sender.class).toProvider(new PlayerSenderProvider<>());
         bind(EntityPlayer.class).toProvider(new PlayerProvider<>());
         bind(EntityPlayerMP.class).toProvider(new PlayerProvider<>());
+        bind(Key.<Set<EntityPlayer>>get(new TypeToken<Set<EntityPlayer>>(){}.getType())).toProvider(new PlayerSetProvider<>());
+        bind(Key.<Set<EntityPlayerMP>>get(new TypeToken<Set<EntityPlayerMP>>(){}.getType())).toProvider(new PlayerSetProvider<>());
+    }
+
+    private static EntityPlayerMP findPlayer(String test) throws ArgumentParseException {
+        test = test.toLowerCase();
+
+        List<EntityPlayerMP> candidates = Lists.newArrayList();
+
+        for (EntityPlayerMP player : Server.getOnlinePlayers()) {
+            String name = player.getGameProfile().getName().toLowerCase();
+            if (name.equalsIgnoreCase(test)) {
+                return player;
+            } else if (name.startsWith(test)) {
+                candidates.add(player);
+            }
+        }
+
+        if (candidates.isEmpty()) {
+            throw new ArgumentParseException(tr("args.noPlayersMatched", test));
+        } else if (candidates.size() == 1) {
+            return candidates.get(0);
+        } else {
+            Joiner joiner = Joiner.on(tr("listSeparator"));
+            throw new ArgumentParseException(tr("args.didYouMean", joiner.join(candidates)));
+        }
     }
 
     private class PlayerProvider<T extends EntityPlayer> implements Provider<T> {
@@ -43,27 +74,7 @@ public class ForgeModule extends AbstractModule {
         @Nullable
         @Override
         public T get(CommandArgs arguments, List<? extends Annotation> modifiers) throws ArgumentException, ProvisionException {
-            String test = arguments.next();
-            List<EntityPlayer> candidates = Lists.newArrayList();
-
-            for (Object object : MinecraftServer.getServer().getConfigurationManager().playerEntityList) {
-                EntityPlayer player = (EntityPlayer) object;
-                String name = player.getGameProfile().getName();
-                if (name.equalsIgnoreCase(test)) {
-                    return (T) player;
-                } else {
-                    candidates.add(player);
-                }
-            }
-
-            if (candidates.isEmpty()) {
-                throw new ArgumentParseException(tr("args.noPlayersMatched", test));
-            } else if (candidates.size() == 1) {
-                return (T) candidates.get(0);
-            } else {
-                Joiner joiner = Joiner.on(tr("listSeparator"));
-                throw new ArgumentParseException(tr("args.didYouMean", joiner.join(candidates)));
-            }
+            return (T) findPlayer(arguments.next());
         }
 
         @Override
@@ -71,7 +82,6 @@ public class ForgeModule extends AbstractModule {
             return ImmutableList.of();
         }
     }
-
 
     private class PlayerSenderProvider<T extends EntityPlayer> implements Provider<T> {
         @Override
@@ -89,6 +99,41 @@ public class ForgeModule extends AbstractModule {
             } else {
                 throw new ArgumentParseException("Only players can use this command.");
             }
+        }
+
+        @Override
+        public List<String> getSuggestions(String prefix) {
+            return ImmutableList.of();
+        }
+    }
+
+    private class PlayerSetProvider<T extends EntityPlayer> implements Provider<Set<T>> {
+        @Override
+        public boolean isProvided() {
+            return false;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Nullable
+        @Override
+        public Set<T> get(CommandArgs arguments, List<? extends Annotation> modifiers) throws ArgumentException, ProvisionException {
+            Set<T> players = Sets.newHashSet();
+            String input;
+
+            do {
+                input = arguments.next();
+                String[] tokens = input.split(",");
+                for (String token : tokens) {
+                    token = token.trim();
+                    if (token.equals("*")) {
+                        players.addAll((Collection<? extends T>) Server.getOnlinePlayers());
+                    } else {
+                        players.add((T) findPlayer(token));
+                    }
+                }
+            } while (!input.isEmpty() && input.charAt(input.length() - 1) == ',');
+
+            return players;
         }
 
         @Override
