425293a9d5439159e46e51bb17c5f965a36ac9d9
sk89q <the.sk89q@gmail.com>
2015-09-07 15:56:26 -0700

    Update language file.

--- src/main/java/com/skcraft/plume/module/Broadcast.java
+++ src/main/java/com/skcraft/plume/module/Broadcast.java
@@ -1,9 +1,9 @@
 package com.skcraft.plume.module;
 
 import com.sk89q.intake.Command;
-import com.sk89q.intake.CommandException;
 import com.sk89q.intake.Require;
 import com.sk89q.intake.parametric.annotation.Switch;
+import com.sk89q.intake.parametric.annotation.Text;
 import com.skcraft.plume.command.Sender;
 import com.skcraft.plume.common.util.module.Module;
 import com.skcraft.plume.util.Messages;
@@ -16,7 +16,7 @@ public class Broadcast {
 
     @Command(aliases = {"broadcast", "bc"}, desc = "Send out a broadcast message")
     @Require("plume.broadcast.bc")
-    public void broadcast(@Sender ICommandSender sender, String msg, @Switch('t') String type) {
+    public void broadcast(@Sender ICommandSender sender, @Text String msg, @Switch('t') String type) {
         if (type == null || type.equalsIgnoreCase("type")) {
             Messages.broadcastInfo(msg);
         } else if (type.equalsIgnoreCase("alert")) {
