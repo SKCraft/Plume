b0a483755ea5a2b1dd4ea0d90ab827ca870d9c01
sk89q <the.sk89q@gmail.com>
2015-09-25 17:10:13 -0700

    Add Locations methods for tile entities.

--- src/main/java/com/skcraft/plume/util/Locations.java
+++ src/main/java/com/skcraft/plume/util/Locations.java
@@ -2,6 +2,7 @@ package com.skcraft.plume.util;
 
 import com.skcraft.plume.common.util.WorldVector3i;
 import net.minecraft.entity.Entity;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.World;
 
@@ -18,8 +19,16 @@ public final class Locations {
         return new Location3i(entity.worldObj, (int) entity.posX, (int) entity.posY, (int) entity.posZ);
     }
 
+    public static Location3i getLocation3i(TileEntity tileEntity) {
+        return new Location3i(tileEntity.getWorldObj(), tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord);
+    }
+
     public static WorldVector3i getWorldVector3i(Entity entity) {
         return getLocation3i(entity).toWorldVector();
+    }
+
+    public static WorldVector3i getWorldVector3i(TileEntity tileEntity) {
+        return getLocation3i(tileEntity).toWorldVector();
     }
 
     public static Location3i getLocation3i(World world, ChunkCoordinates coords) {
