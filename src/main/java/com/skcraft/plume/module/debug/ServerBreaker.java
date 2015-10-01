edb60e31ddd2a89fe531436289b348998c7e501d
Albert Pham <the.sk89q@gmail.com>
2015-10-01 14:48:53 -0700

    Add command to test stall a server during a tile entity tick.

--- src/main/java/com/skcraft/plume/module/debug/ServerBreaker.java
+++ src/main/java/com/skcraft/plume/module/debug/ServerBreaker.java
@@ -3,7 +3,11 @@ package com.skcraft.plume.module.debug;
 import com.sk89q.intake.Command;
 import com.sk89q.intake.Require;
 import com.skcraft.plume.command.Sender;
+import com.skcraft.plume.common.util.Stopwatch;
+import com.skcraft.plume.common.util.event.Order;
+import com.skcraft.plume.common.util.event.Subscribe;
 import com.skcraft.plume.common.util.module.Module;
+import com.skcraft.plume.event.tick.TileEntityTickEvent;
 import com.skcraft.plume.util.Messages;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent.ServerTickEvent;
@@ -17,6 +21,7 @@ import static com.skcraft.plume.common.util.SharedLocale.tr;
 public class ServerBreaker {
 
     private long stall = 0;
+    private long stallTick = 0;
     private boolean crash = false;
 
     @Command(aliases = "stallserver", desc = "Stall the server")
@@ -26,6 +31,13 @@ public class ServerBreaker {
         stall = time > 0 ? time : 1;
     }
 
+    @Command(aliases = "stalltileentity", desc = "Stall the next tile entity")
+    @Require("plume.serverbreaker.stall")
+    public void stallNextTileEntity(@Sender ICommandSender sender, int time) {
+        Messages.broadcast(Messages.info(tr("serverBreaker.stallTileEntityCalled", sender.getCommandSenderName())));
+        stallTick = time > 0 ? time : 1;
+    }
+
     @Command(aliases = "crashserver", desc = "Crash the server")
     @Require("plume.serverbreaker.crash")
     public void crashServer(@Sender ICommandSender sender) {
@@ -33,13 +45,43 @@ public class ServerBreaker {
         crash = true;
     }
 
+    @Subscribe(order = Order.VERY_LATE)
+    public void onTileEntityTick(TileEntityTickEvent event) {
+        if (stallTick > 0) {
+            long time = stallTick;
+            stallTick = 0;
+
+            // Stall in a stopwatch because it's only at this time that we end up in the try {} catch
+            event.getStopwatches().add(new Stopwatch() {
+                @Override
+                public void start() {
+                    log.info("Proceeding to stall the server due to /stalltileentity usage...");
+                    try {
+                        for (int i = 0; i < time; i++) {
+                            Thread.sleep(1000);
+                        }
+                    } catch (InterruptedException e) {
+                        log.info("Server stall interrupted!");
+                        Thread.interrupted();
+                    }
+                }
+
+                @Override
+                public void stop() {
+                }
+            });
+        }
+    }
+
     @SubscribeEvent
     public void onServerTick(ServerTickEvent event) {
         if (stall > 0) {
             log.info("Proceeding to stall the server due to /stallserver usage...");
             try {
-                Thread.sleep(stall * 1000);
-                stall = 0;
+                while (stall > 0) {
+                    Thread.sleep(1000);
+                    stall--;
+                }
             } catch (InterruptedException e) {
                 log.info("Server stall interrupted!");
                 Thread.interrupted();
