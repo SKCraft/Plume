e34781063c5023781b8bd4143fb7116fe299e48d
wizjany <wizjany@gmail.com>
2015-10-19 18:49:01 -0400

    Add persistence to highlighter preferences. Currently in uuid-named files in config/Plume/chathighlights until sk writes Prefs API.

--- src/main/java/com/skcraft/plume/module/chat/ChatHighlighter.java
+++ src/main/java/com/skcraft/plume/module/chat/ChatHighlighter.java
@@ -2,6 +2,7 @@ package com.skcraft.plume.module.chat;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Maps;
+import com.google.common.io.Files;
 import com.google.inject.Inject;
 import com.sk89q.intake.Command;
 import com.sk89q.intake.Require;
@@ -11,6 +12,7 @@ import com.sk89q.intake.parametric.annotation.Text;
 import com.skcraft.plume.command.Sender;
 import com.skcraft.plume.common.UserId;
 import com.skcraft.plume.common.util.config.Config;
+import com.skcraft.plume.common.util.config.DataDir;
 import com.skcraft.plume.common.util.config.InjectConfig;
 import com.skcraft.plume.common.util.module.Module;
 import com.skcraft.plume.util.Messages;
@@ -20,6 +22,10 @@ import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraft.entity.player.EntityPlayer;
 import ninja.leaping.configurate.objectmapping.Setting;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
 import java.util.Map;
 
 import static com.skcraft.plume.common.util.SharedLocale.tr;
@@ -28,10 +34,14 @@ import static com.skcraft.plume.common.util.SharedLocale.tr;
         desc = "Highlights and notifies players when certain keywords appear in chat.")
 public class ChatHighlighter {
 
+    @Inject @DataDir private File plumeDir;
+    private File highlightDir;
     @Inject private ChatListener listener;
     @InjectConfig("chat_highlighter") private Config<HighlighterConfig> config;
     @Inject private void init() {
         listener.highlighter = this;
+        highlightDir = new File(plumeDir, "chathighlights" + File.separator);
+        highlightDir.mkdirs();
     }
 
     private Map<UserId, String[]> highlights = Maps.newHashMap();
@@ -51,6 +61,8 @@ public class ChatHighlighter {
         } else {
             sender.addChatMessage(Messages.info(tr("highlights.set", Joiner.on(tr("listSeparator") + " ").join(keywords))));
         }
+        writeFile(new File(highlightDir, uid.getUuid().toString()), isSoundEnabled(sender),
+                getSound(sender), keywords.length > 0 ? Joiner.on(' ').join(keywords) : "");
     }
 
     @Command(aliases = "highlightsound",
@@ -63,7 +75,8 @@ public class ChatHighlighter {
     @Require("plume.chathighlight")
     public void highlightSound(@Sender EntityPlayer sender, @Optional("") String sound, @Switch('d') boolean toggle) {
         UserId uid = Profiles.fromPlayer(sender);
-        sounds.put(uid, !toggle || !sound.isEmpty());
+        boolean enabled = !toggle || !sound.isEmpty();
+        sounds.put(uid, enabled);
         String soundName = sound.isEmpty() ? config.get().soundName : sound;
         soundNames.put(uid, soundName);
         if (!toggle) {
@@ -71,17 +84,41 @@ public class ChatHighlighter {
         } else {
             sender.addChatMessage(Messages.info(tr("highlights.sound.off")));
         }
+        writeFile(new File(highlightDir, uid.getUuid().toString()), enabled, soundName, Joiner.on(' ').join(getKeywords(sender)));
     }
 
     @SubscribeEvent
     public void onPlayerLogin(PlayerLoggedInEvent event) {
-        // TODO persistance
         UserId uid = Profiles.fromPlayer(event.player);
-        highlights.put(uid, new String[] { event.player.getCommandSenderName() });
-        sounds.put(uid, config.get().soundEnabled);
-        soundNames.put(uid, config.get().soundName);
+
+        File userFile = new File(highlightDir, uid.getUuid().toString());
+        boolean enabled = config.get().soundEnabled;
+        String sound =  config.get().soundName;
+        String keywords = event.player.getCommandSenderName();
+        if (!userFile.exists()) {
+            writeFile(userFile, enabled, sound, keywords);
+        } else {
+            try {
+                List<String> lines = Files.readLines(userFile, Charset.forName("UTF-8"));
+                enabled = Boolean.valueOf(lines.get(0));
+                sound = lines.get(1);
+                keywords = lines.get(2);
+            } catch (IOException ignored) {
+            }
+        }
+        highlights.put(uid, keywords.split(" "));
+        sounds.put(uid, enabled);
+        soundNames.put(uid, sound);
     }
 
+    private static void writeFile(File file, boolean enabled, String sound, String keywords) {
+        try {
+            if (!file.exists()) file.createNewFile();
+            String data = enabled + "\n" + sound + "\n" + keywords;
+            Files.write(data, file, Charset.forName("UTF-8"));
+        } catch (IOException ignored) { // settings just won't persist
+        }
+    }
     public String getSound(EntityPlayer recipient) {
         return soundNames.get(Profiles.fromPlayer(recipient));
     }
