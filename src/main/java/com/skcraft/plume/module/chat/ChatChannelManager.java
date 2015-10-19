f5ae125bac6129fb327a5945a91eb4178b3d37bf
wizjany <wizjany@gmail.com>
2015-10-18 20:34:25 -0400

    Consolidate channel methods, used Deferreds.

--- src/main/java/com/skcraft/plume/module/chat/ChatChannelManager.java
+++ src/main/java/com/skcraft/plume/module/chat/ChatChannelManager.java
@@ -2,13 +2,15 @@ package com.skcraft.plume.module.chat;
 
 import com.google.inject.Inject;
 import com.skcraft.plume.common.UserId;
+import com.skcraft.plume.common.util.concurrent.Deferred;
+import com.skcraft.plume.common.util.concurrent.Deferreds;
 import com.skcraft.plume.common.util.module.AutoRegister;
 import com.skcraft.plume.util.Messages;
+import com.skcraft.plume.util.concurrent.BackgroundExecutor;
+import com.skcraft.plume.util.concurrent.TickExecutorService;
 import com.skcraft.plume.util.profile.Profiles;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentTranslation;
-import net.minecraft.util.ChatStyle;
-import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
 
 import javax.annotation.Nullable;
@@ -22,8 +24,9 @@ import static com.skcraft.plume.common.util.SharedLocale.tr;
 @AutoRegister
 public class ChatChannelManager {
 
-    @Inject
-    private ChatListener listener;
+    @Inject private TickExecutorService tickExecutorService;
+    @Inject private BackgroundExecutor bgExecutor;
+    @Inject private ChatListener listener;
     @Inject private void init() {
         listener.manager = this;
     }
@@ -31,9 +34,12 @@ public class ChatChannelManager {
     private Map<UserId, String> users = new HashMap<>();
     private Map<String, List<UserId>> channels = new HashMap<>();
 
-    private boolean addPlayerToChannel(String name, EntityPlayer player) {
+    private boolean addPlayerToChannel(String name, EntityPlayer player) throws ChannelException {
         boolean success;
         UserId uid = Profiles.fromPlayer(player);
+        if (users.get(uid) != null) {
+            throw new AlreadyInChannelException();
+        }
         if (channels.containsKey(name)) {
             success = channels.get(name).add(uid);
         } else {
@@ -46,43 +52,64 @@ public class ChatChannelManager {
         return success;
     }
 
-    private boolean removePlayerFromChannel(EntityPlayer player) {
+    private void removePlayerFromChannel(EntityPlayer player) throws ChannelException {
         UserId uid = Profiles.fromPlayer(player);
         String channel = users.get(uid);
-        List<UserId> uidlist = channels.get(channel);
-        boolean success;
-        success = uidlist.remove(uid);
-        if (success) {
-            channels.put(channel, uidlist);
-            users.remove(uid);
+        if (channel == null) {
+            throw new NoChannelException();
         }
-        return success;
+        List<UserId> uidlist = channels.get(channel);
+        if (!uidlist.remove(uid)) {
+            throw new ChannelException();
+        }
+        channels.put(channel, uidlist);
+        users.remove(uid);
     }
 
     public void joinChannel(EntityPlayer player, String channel) {
-        if (isInChatChannel(player)) {
-            player.addChatMessage(Messages.info(tr("chatChannel.alreadySubscribed")));
-            return;
-        }
+        Deferred<?> deferred = Deferreds
+                .when(() -> {
+                    addPlayerToChannel(channel, player);
+                    return channel;
+                }, bgExecutor.getExecutor())
+                .done(chanName -> {
+                    sendMessageToChannel(channel, Messages.info(tr("chatChannel.join.success", player.getDisplayName(), channel)));
+                }, tickExecutorService)
+                .fail(e -> {
+                    if (e instanceof AlreadyInChannelException) {
+                        player.addChatMessage(Messages.info(tr("chatChannel.alreadySubscribed")));
+                    } else if (e instanceof ChannelException) {
+                        player.addChatMessage(Messages.error(tr("chatChannel.join.error")));
+                    } else {
+                        player.addChatMessage(Messages.exception(e));
+                    }
+                }, tickExecutorService);
 
-        // TODO defer
-        boolean success = addPlayerToChannel(channel, player);
-        player.addChatMessage(Messages.info(tr("chatChannel.join.self", channel)));
-        sendMessageToChannel(channel, Messages.info(tr("chatChannel.join.other", player.getDisplayName(), channel)));
-        //player.addChatMessage(Messages.error(tr("chatChannel.join.error)));
+        bgExecutor.notifyOnDelay(deferred, player);
     }
 
     public void leaveChannel(EntityPlayer player) {
-        if (!isInChatChannel(player)) {
-            player.addChatMessage(Messages.info(tr("chatChannel.leave.noChannel")));
-            return;
-        }
         String channel = users.get(Profiles.fromPlayer(player));
-        // TODO defer
-        boolean success = removePlayerFromChannel(player);
-        player.addChatMessage(Messages.info(tr("chatChannel.leave.self", channel)));
-        sendMessageToChannel(channel, Messages.info(tr("chatChannel.leave.other", player.getDisplayName(), channel)));
-        //player.addChatMessage(Messages.error(tr("chatChannel.leave.error")));
+
+        Deferred<?> deferred = Deferreds
+                .when(() -> {
+                    removePlayerFromChannel(player);
+                    return channel;
+                }, bgExecutor.getExecutor())
+                .done(chanName -> {
+                    sendMessageToChannel(channel, Messages.info(tr("chatChannel.leave.success", player.getDisplayName(), channel)));
+                }, tickExecutorService)
+                .fail(e -> {
+                    if (e instanceof NoChannelException) {
+                        player.addChatMessage(Messages.info(tr("chatChannel.leave.noChannel")));
+                    } else if (e instanceof ChannelException) {
+                        player.addChatMessage(Messages.error(tr("chatChannel.leave.error")));
+                    } else {
+                        player.addChatMessage(Messages.exception(e));
+                    }
+                }, tickExecutorService);
+
+        bgExecutor.notifyOnDelay(deferred, player);
     }
 
     public boolean isInChatChannel(EntityPlayer player) {
@@ -99,7 +126,6 @@ public class ChatChannelManager {
         return getChannel(Profiles.fromPlayer(player));
     }
 
-    private static final ChatStyle CLEAR = new ChatStyle().setColor(EnumChatFormatting.RESET);
     public void sendChatToChannel(String channel, ChatComponentTranslation msg) {
         if (channels.containsKey(channel)) {
             for (UserId u : channels.get(channel)) {
@@ -118,5 +144,12 @@ public class ChatChannelManager {
                 player.addChatComponentMessage(message);
             }
         }
+    }
+
+    private static class ChannelException extends Exception {
+    }
+    private static class NoChannelException extends ChannelException {
+    }
+    private static class AlreadyInChannelException extends ChannelException {
     }
 }
